7350f2d924a03986af748dc516697a691a04057e
foxca <foxca@LAPTOP-UTQBBQF9>
2022-07-01 20:11:05 -0300

    Adicionar arquivos de projeto.

---
+++ Prova4Djalma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova4Djalma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NOME: Rita de Cassia Franca Faria      RA: 6322054
+            // 4 – Utilizando estrutura FOR, escrever um programa para exibir os números de 1 até 50 na tela.
+            int i;
+
+            for (i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i * 1);
+            }
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
